826bc3c09e15fb1a6e30af75b8fe30ef0d15c248
Sultan1211 <204g1a3254@srit.ac.in>
2025-01-22 18:06:13 +0530

    changes made

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,6 @@ import FeaturedCourses from "@/components/FeaturedCourses";
 import HeroSection from "@/components/HeroSection";
 import MusicSchoolTestimonials from "@/components/TestimonialCards";
 import WhyChooseUs from "@/components/WhyChooseUs";
-import Upcomingwebinars from "@/components/Upcomingwebinars";
 import Image from "next/image";
 import Instructors from "@/components/Instructors";
 import Footer from "@/components/Footer";
@@ -14,7 +13,7 @@ export default function Home() {
       <FeaturedCourses/>
       <WhyChooseUs/>
       <MusicSchoolTestimonials/>
-      <Upcomingwebinars/>
+      <UpcomingWebinars/>
       <Instructors/>
       <Footer/>
    </main>
--- src/components/UpcomingWebinars.jsx
+++ src/components/UpcomingWebinars.jsx
@@ -1,10 +1,9 @@
 'use client'
-import Link from 'next/link'
-import React from 'react'
-import { HoverEffect } from './ui/card-hover-effect';
-
+import Link from "next/link"
+import { HoverEffect } from "./ui/card-hover-effect";
 
 function UpcomingWebinars() {
+
   const featuredWebinars = [
     {
       title: 'Understanding Music Theory',
@@ -50,30 +49,34 @@ function UpcomingWebinars() {
       isFeatured: true,
     },
   ];
+
   return (
-    <div className='p-12 bg-grey-900'>
-      <div className='max-w-7xl mx-auto px-4 sm:px-6'>
-        <div className='text-center'>
-          <h2 className='text-base text-teal-500 tracking-wide font-semibold'>FEATURED WEBINARS</h2>
-          <p className='mt-2 text-3xl font-extrabold tracking-tight leading-6 sm:4xl'>Enhance your musical Journey</p>
+    <div className="p-12 bg-gray-900">
+      <div className="max-w-7xl mx-auto px-4 sm:px-6">
+        <div className="text-center">
+          <h2 className="text-base text-teal-600 font-semibold tracking-wide uppercase">FEATURED WEBINARS</h2>
+          <p className="mt-2 text-3xl leading-8 font-extrabold tracking-tight text-white sm:text-4xl">Enhance Your Musical Journey</p>
         </div>
-        <div className='mt-10'>
-          <HoverEffect items={featuredWebinars.map(webinar => (
+
+        <div className="mt-10">
+          <HoverEffect
+          items={featuredWebinars.map(webinar => (
             {
               title: webinar.title,
               description: webinar.description,
               link: '/'
-            }))} />
+            }
+          ))}
+          />
         </div>
-        <div className='mt-10 text-center'>
-          <Link href={"/courses"} className='mt-20 flex justify-center items-center'>
-            <button className="px-4 py-2 rounded-md border border-neutral-300 bg-neutral-100 text-black text-sm hover:-translate-y-1 transform transition duration-200 hover:shadow-md">
-              View All Courses
-            </button>
 
+        <div className="mt-10 text-center">
+          <Link href={"/"}
+          className="px-4 py-2 rounded border border-neutral-600 text-neutral-700 bg-white hover:bg-gray-100 transition duration-200"
+          >
+            View All webinars
           </Link>
         </div>
-
       </div>
     </div>
   )
